b6c2cc4e7a91e5b09734d3695dfd35bd30220a6f
laurentwambura <a.laurentwambura@gmail.com>
2023-07-16 19:57:49 +0300

    solve index bug

--- two_sum.py
+++ two_sum.py
@@ -2,9 +2,9 @@ def two_sum(nums, target):
     for num_index,num in enumerate(nums):
         val = target - num
         try:
-            val_index = nums.index(val)
+            val_index = nums.index(val, num_index + 1)
             return [num_index,val_index]
         except ValueError:
             continue
 
-print(two_sum(nums = [3,2,4], target = 6))
+print(two_sum(nums = [3,3], target = 6))
